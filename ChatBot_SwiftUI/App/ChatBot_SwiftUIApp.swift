842d53ddceaadfef5fb739c901ac122cfd398573
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-08-10 15:01:49 +0400

    splash Screen done

--- ChatBot_SwiftUI/App/ChatBot_SwiftUIApp.swift
+++ ChatBot_SwiftUI/App/ChatBot_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ChatBot_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
+            SplashView()
         }
     }
 }
--- ChatBot_SwiftUI/UI/Splash/SplashView.swift
+++ ChatBot_SwiftUI/UI/Splash/SplashView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct SplashView: View {
+    
+    @State var isActive: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            if self.isActive {
+                OnboardingView()
+            } else {
+                Color.accentColor.edgesIgnoringSafeArea(.all)
+                Image("BotLogoicon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 262, height: 262)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
     }
+        
 }
 
 #Preview {
