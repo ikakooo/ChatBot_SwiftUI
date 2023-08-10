6715430a80621ac0af6d674f182eaeeef3fb3e02
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-08-10 15:38:50 +0400

    OnboardingView done

--- ChatBot_SwiftUI/UI/Onboarding/OnboardingView.swift
+++ ChatBot_SwiftUI/UI/Onboarding/OnboardingView.swift
@@ -9,7 +9,56 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+                .frame(height: 60)
+            Text("You AI Assistant")
+                .font(.title)
+                .foregroundStyle(Color.accentColor)
+            Spacer()
+                .frame(height: 14)
+            Text("Using this software,you can ask you questions and receive articles using artificial intelligence assistant")
+                .font(.subheadline)
+                .foregroundStyle(Color.gray)
+                .padding(.leading, 66)
+                .padding(.trailing, 66)
+            Spacer()
+                .frame(height: 80)
+            Image("OnboardingImage")
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+
+                    ZStack {
+                        HStack {
+                            Spacer()
+                            Text("Continue")
+                                .font(.title2)
+                                .fontWeight(.medium)
+                            Spacer()
+                        }
+                        HStack {
+                          Spacer()
+                            Image(systemName: "arrow.right")
+                                .resizable()
+                                .frame(width: 24, height: 16)
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(
+                        ZStack {
+                            RoundedRectangle(
+                                cornerRadius: 20,
+                                style: .continuous
+                            )
+                            .fill(.blue)
+                        })
+            })
+            .padding(.leading, 21)
+            .padding(.trailing, 21)
+        }
     }
 }
 
